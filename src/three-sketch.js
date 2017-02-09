f8431d2cdf771fcbd43832907ee56f5a608950be
Chris Parker <parker@byhook.com>
2017-02-09 12:35:09 -0500

    use three js event dispatcher and clock instead of reimplementing, use event flyweights, use cancelAnimationFrame, cleanup

--- src/three-sketch.js
+++ src/three-sketch.js
@@ -1,31 +1,44 @@
-import Emitter from 'component-emitter';
 import { insertCSS } from './insert-css';
-import { WebGLRenderer, PerspectiveCamera, Scene, Vector2 } from 'three';
+import { EventDispatcher, WebGLRenderer, PerspectiveCamera, Scene, Clock, Vector2 } from 'three';
 
 // When the module is loaded, make sure there is some CSS to allow a fullscreen
 // borderless canvas without scrollbars.
 insertCSS('html, body { margin: 0px; width: 100%; height: 100%; overflow: hidden; }');
+
+const requestAnimationFrame = window.requestAnimationFrame ||
+                              window.mozRequestAnimationFrame ||
+                              window.webkitRequestAnimationFrame;
+
+if (requestAnimationFrame === undefined) {
+
+  throw new Error('window.requestAnimationFrame is missing');
+
+}
+
+const cancelAnimationFrame = window.cancelAnimationFrame ||
+                             window.mozCancelAnimationFrame ||
+                             window.webkitCancelAnimationFrame;
+
+if (cancelAnimationFrame === undefined) {
+
+  throw new Error('window.cancelAnimationFrame is missing');
+
+}
 
 /**
  * Create a three.js sketch.
  */
 export function Sketch() {
 
-  // Initialize some three.js objects.
   const renderer = new WebGLRenderer({ antialias: true });
-  const canvas   = renderer.domElement;
-  const scene    = new Scene();
-  const camera   = new PerspectiveCamera(45, 1, 0.1, 1000);
-  const mouse    = new Vector2(0, 0);
+  const camera = new PerspectiveCamera(45, 1, 0.1, 1000);
+  const scene = new Scene();
+  const mouse = new Vector2(0, 0);
+  const clock = new Clock(false);
 
-  let time = 0;
+  let rafID = null; // the return value of requestAnimationFrame
 
-  // Set the default camera as a property of the scene. The camera property on
-  // the scene will be used to render the scene, so if the user wants to use a
-  // different camera they can just replace this property.
-  scene.camera = camera;
-
-  const sketch = Object.create(Emitter.prototype, {
+  const sketch = Object.create(EventDispatcher.prototype, {
 
     renderer: {
       value: renderer,
@@ -33,7 +46,7 @@ export function Sketch() {
     },
 
     canvas: {
-      value: canvas,
+      value: renderer.domElement,
       writable: false,
     },
 
@@ -63,59 +76,58 @@ export function Sketch() {
     },
 
     time: {
-      get: () => time,
+      get: () => clock.getElapsedTime(),
       set: () => { throw new Error('The "time" property is readonly.') },
     }
 
   });
 
-  // A mutable boolean that we can use to break the render loop.
-  let running = false;
+  // Make some event flyweights so we aren't allocating objects every frame.
+
+  const updateEventFlyweight = { type: 'update' };
+  const resizeEventFlyweight = { type: 'resize' };
+  const mouseEventFlyweight  = { type: 'mousemove' };
+
+  // Set the default camera as a property of the scene. The camera property on
+  // the scene will be used to render the scene, so if the user wants to use a
+  // different camera they can just replace this property.
+
+  scene.camera = camera;
 
   /**
    * Start running the sketch.
    *
-   * @param {Function} [requestAnimationFrame] - A function to request the next
-   *   animation frame. `window.requestAnimationFrame` is used by default, but
-   *   you might want to use a different function (like when using WebVR).
+   * @param {Function} [raf] - A function to request the next animation frame.
+   * `window.requestAnimationFrame` is used by default, but you might want to
+   * use a different function (like when using WebVR).
    */
-  sketch.start = function(requestAnimationFrame = window.requestAnimationFrame) {
+  sketch.start = function(raf = requestAnimationFrame) {
 
-    if (running) {
+    if (rafID !== null) throw new Error("The sketch is already running.");
 
-      throw new Error("The sketch is already running.");
+    clock.start();
+
+    sketch.onResize();
+
+    window.addEventListener('resize', sketch.onResize);
+    window.addEventListener('mousemove', sketch.onMousemove);
+
+    function loop() {
+
+      const dt = updateEventFlyweight.dt = clock.getDelta();
+      const time = updateEventFlyweight.time = clock.getElapsedTime();
+
+      // update before rendering
+      sketch.dispatchEvent(updateEventFlyweight);
+      sketch.render(dt, time);
+
+      rafID = raf(loop);
 
     }
 
-    running = true;
+    rafID = raf(loop);
 
-    window.addEventListener('resize', onResize);
-    window.addEventListener('mousemove', onMousemove);
-
-    // Resize the renderer/camera before emitting the start event, so that any
-    // initialization is done at the correct resolution.
-    onResize();
-
-    sketch.emit('start', sketch);
-
-    const loop = function (currentTime) {
-
-      if (!running) return;
-
-      // Update the delta and current time.
-      const deltaTime = currentTime - time;
-      time = currentTime;
-
-      // Emit an update event for any side effects, then render the scene.
-      sketch.emit('update', deltaTime, sketch);
-      sketch.render(deltaTime);
-
-      requestAnimationFrame(loop);
-
-    };
-
-    // Start the render loop.
-    requestAnimationFrame(loop);
+    sketch.dispatchEvent({ type: 'start' });
 
   };
 
@@ -124,63 +136,85 @@ export function Sketch() {
    */
   sketch.stop = function () {
 
-    window.removeEventListener('resize', onResize);
-    window.removeEventListener('mousemove', onMousemove);
+    if (rafID === null) return;
 
-    running = false;
+    clock.stop();
+
+    window.removeEventListener('resize', sketch.onResize);
+    window.removeEventListener('mousemove', sketch.onMousemove);
+
+    cancelAnimationFrame(rafID);
+
+    rafID = null;
+
+    sketch.dispatchEvent({ type: 'stop' });
 
   };
 
-  sketch.render = function () {
+  /**
+   * Called on each animation frame.
+   *
+   * @param {Number} dt - The fractional seconds since the last render.
+   * @param {Number} time - The fractional seconds since the sketch started.
+   */
+  sketch.render = function (dt, time) {
 
     if (!scene.camera || !scene.camera.isCamera) {
 
       console.warn("A custom camera was assigned to `scene.camera` but it isn't a recognizable three.js camera object. Falling back to the default camera.");
-      renderer.render(scene, camera);
-
-    } else {
-
-      renderer.render(scene, scene.camera);
+      scene.camera = camera;
 
     }
 
+    renderer.render(scene, scene.camera);
+
   };
 
-  const onResize = function () {
+  /**
+   * Called when the window resizes.
+   */
+  sketch.onResize = function () {
 
     const width = window.innerWidth;
     const height = window.innerHeight;
-    const density = window.devicePixelRatio || 1;
+    const pratio = window.devicePixelRatio || 1;
 
-    renderer.setPixelRatio(density);
+    renderer.setPixelRatio(pratio);
     renderer.setSize(width, height, true);
 
-    // Update the default camera if it is still being used to render the scene.
-
     if (scene.camera === camera) {
+
+      // update the default camera if it hasn't been overwritten
 
       camera.aspect = width / height;
       camera.updateProjectionMatrix();
 
     }
 
-    sketch.emit('resize', { width, height }, sketch);
+    resizeEventFlyweight.width = width;
+    resizeEventFlyweight.height = height;
+
+    sketch.dispatchEvent(resizeEventFlyweight);
 
   };
 
-  const onMousemove = function (event) {
+  /**
+   * Called when the mouse moves.
+   */
+  sketch.onMousemove = function (event) {
 
-    const width = canvas.clientWidth;
-    const height = canvas.clientHeight;
-    const density = window.devicePixelRatio || 1;
+    const width = window.innerWidth;
+    const height = window.innerHeight;
 
-    // Normalize the mouse coordinate to [-1, 1].
+    // normalize the mouse coordinate to [-1, 1]
     const normalizedX = 2 * (event.clientX / width) - 1;
     const normalizedY = 2 * (1 - event.clientY / height) - 1;
 
     mouse.set(normalizedX, normalizedY);
 
-    sketch.emit('mousemove', mouse, sketch);
+    mouseEventFlyweight.mouse = mouse;
+
+    sketch.dispatchEvent(mouseEventFlyweight);
 
   };
 
